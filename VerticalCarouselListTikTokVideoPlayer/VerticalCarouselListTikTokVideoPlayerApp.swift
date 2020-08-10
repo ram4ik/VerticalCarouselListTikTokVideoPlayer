636dbb2f781a660ccbc86cfaf005a0d39fd5491f
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2020-08-10 09:59:27 +0300

    Initial Commit

---
+++ VerticalCarouselListTikTokVideoPlayer/VerticalCarouselListTikTokVideoPlayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  VerticalCarouselListTikTokVideoPlayerApp.swift
+//  VerticalCarouselListTikTokVideoPlayer
+//
+//  Created by ramil on 10.08.2020.
+//
+
+import SwiftUI
+
+@main
+struct VerticalCarouselListTikTokVideoPlayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
