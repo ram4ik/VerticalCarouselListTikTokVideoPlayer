c4833005f2dfc89574628805c23ea22805e00986
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2020-08-10 10:04:55 +0300

    init

--- VerticalCarouselListTikTokVideoPlayer/ContentView.swift
+++ VerticalCarouselListTikTokVideoPlayer/ContentView.swift
@@ -9,8 +9,27 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            Home()
+                .preferredColorScheme(.dark)
+                .navigationTitle("")
+                .navigationBarHidden(true)
+        }
+    }
+}
+
+struct Home: View {
+    @State var index = 0
+    
+    var body: some View {
+        TabView(selection: $index) {
+            ForEach(1...10, id: \.self) { i in
+                Color.red
+                    .padding()
+            }
+        }
+        .tabViewStyle(PageTabViewStyle(indexDisplayMode: .never))
+        .background(Color.black.ignoresSafeArea(.all, edges: .all))
     }
 }
 
